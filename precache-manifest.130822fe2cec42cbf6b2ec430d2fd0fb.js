98c218e44a4ea847e130e2d495bda65f62c62891
Anand Raj <anandraj7008@gmail.com>
2025-08-15 16:36:37 +0530

    Updates

---
+++ precache-manifest.130822fe2cec42cbf6b2ec430d2fd0fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3bc9ebe444dd5eee453f57e91399f7",
+    "url": "/anandraj123-commit/apurvasoftwareinternship.git/index.html"
+  },
+  {
+    "revision": "42f64489585f0bb9af92",
+    "url": "/anandraj123-commit/apurvasoftwareinternship.git/static/css/2.95ed3913.chunk.css"
+  },
+  {
+    "revision": "962a4b3721803cc86fb1",
+    "url": "/anandraj123-commit/apurvasoftwareinternship.git/static/css/main.019cd9c2.chunk.css"
+  },
+  {
+    "revision": "42f64489585f0bb9af92",
+    "url": "/anandraj123-commit/apurvasoftwareinternship.git/static/js/2.1432eab5.chunk.js"
+  },
+  {
+    "revision": "4dc7554b7036df4c9d79b9b617564329",
+    "url": "/anandraj123-commit/apurvasoftwareinternship.git/static/js/2.1432eab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962a4b3721803cc86fb1",
+    "url": "/anandraj123-commit/apurvasoftwareinternship.git/static/js/main.3e031dd5.chunk.js"
+  },
+  {
+    "revision": "809f17a9d3f44ab6b24b",
+    "url": "/anandraj123-commit/apurvasoftwareinternship.git/static/js/runtime-main.be4885a9.js"
+  }
+]);
